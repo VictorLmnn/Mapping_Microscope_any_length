e06bf175938a90409474b9e6ab7a5c5422f91574
VictorLmnn <69306290+VictorLmnn@users.noreply.github.com>
2020-08-06 15:26:57 +0200

    MM_any_length hinzugefügt

--- Mapping_Microscope_any_length.py
+++ Mapping_Microscope_any_length.py
@@ -28,11 +28,11 @@ for image in image_path_list:
     null[i]=cv2.resize(cv2.imread(image), dsize=(0, 0), fx=0.2, fy=0.2)
 
 null1 = []
-for i in range(17):
+for i in range(int((len(null))**(.5))):
     null1.append([])
 
-    for j in range(16):
-        null1[i].append(null[i*17 + j])
+    for j in range(int((len(null))**(.5))-1):
+        null1[i].append(null[i*int((len(null))**(.5)) + j])
 
 
 #print(null[0:1])
@@ -52,7 +52,7 @@ def concat_tile(im_list_2d):
 
 #fullimage = concat_tile([null[0:17],null[17:34],null[34:51],null[51:68],null[68:85],null[85:102],null[102:119],null[119:136]])
 fullimage = concat_tile(null1)
-cv2.imwrite('fullimage.jpg', fullimage)
+cv2.imwrite('fullimage_any_length.jpg', fullimage)
 cv2.imshow("fullimage.jpg",fullimage)
 cv2.waitKey()
 # plt.imshow(fullimage)
