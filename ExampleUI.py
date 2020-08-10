8263837b4cad005f6ec24bb706124ba366d639c7
VictorLmnn <69306290+VictorLmnn@users.noreply.github.com>
2020-08-10 14:09:27 +0200

    Update ExampleUI.py

--- ExampleUI.py
+++ ExampleUI.py
@@ -8,15 +8,7 @@ import tkinter as tk
 from tkinter import filedialog
 
 
-root = tk.Tk()
-root.withdraw() #use to hide tkinter window
-
-def search_for_file_path ():
-    currdir = os.getcwd()
-    tempdir = filedialog.askdirectory(parent=root, initialdir=currdir, title='Please select a directory')
-    if len(tempdir) > 0:
-        print ("You chose: %s" % tempdir)
-    return tempdir
+master = tk.Tk()
 
 def input():
     input_path = tk.filedialog.askopenfilename()
@@ -24,18 +16,23 @@ def input():
     input_entry.insert(0, input_path)  # Insert the 'path'
 
 
+def output():
+    path = tk.filedialog.askopenfilename()
+    input_entry.delete(1, tk.END)  # Remove current text in entry
+    input_entry.insert(0, path)  # Insert the 'path'
 
-top_frame = tk.Frame(root)
-bottom_frame = tk.Frame(root)
-line = tk.Frame(root, height=1, width=400, bg="grey80", relief='groove')
 
-input_path = tk.Label(top_frame, text="Input Folder Path:")
+top_frame = tk.Frame(master)
+bottom_frame = tk.Frame(master)
+line = tk.Frame(master, height=1, width=400, bg="grey80", relief='groove')
+
+input_path = tk.Label(top_frame, text="Input File Path:")
 input_entry = tk.Entry(top_frame, text="", width=40)
 browse1 = tk.Button(top_frame, text="Browse", command=input)
 
-fullimage_name = tk.Label(bottom_frame, text="Fullimage Name")
+output_path = tk.Label(bottom_frame, text="Output File Path:")
 output_entry = tk.Entry(bottom_frame, text="", width=40)
-
+browse2 = tk.Button(bottom_frame, text="Browse", command=output)
 
 begin_button = tk.Button(bottom_frame, text='Begin!')
 
@@ -47,10 +44,11 @@ input_path.pack(pady=5)
 input_entry.pack(pady=5)
 browse1.pack(pady=5)
 
-fullimage_name.pack(pady=5)
+output_path.pack(pady=5)
 output_entry.pack(pady=5)
+browse2.pack(pady=5)
 
 begin_button.pack(pady=20, fill=tk.X)
 
 
-root.mainloop()
+master.mainloop()
