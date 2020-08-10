d33e23ebc461663128dd413f3df49f3e4b11393d
VictorLmnn <69306290+VictorLmnn@users.noreply.github.com>
2020-08-10 15:23:28 +0200

    simples UI Merge erstellt

--- ExampleUI.py
+++ ExampleUI.py
@@ -6,49 +6,74 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import tkinter as tk
 from tkinter import filedialog
-
+import time
 
 master = tk.Tk()
 
-def input():
-    input_path = tk.filedialog.askopenfilename()
-    input_entry.delete(1, tk.END)  # Remove current text in entry
-    input_entry.insert(0, input_path)  # Insert the 'path'
+
+def input ():
+    currdir = os.getcwd()
+    tempdir = filedialog.askdirectory(parent=master, initialdir=currdir, title='Please select a directory')
+    return tempdir
+# def input():
+#     input_path = tk.filedialog.askdirectory()
+#     input_entry.delete(1, tk.END)  # Remove current text in entry
+#     input_entry.insert(0, input_path)  # Insert the 'path'
+
+def fullimage_merge ():
+
+    image_path_list = []
+
+    imageDir = input()
+    #create a list all files in directory and
+    #append files with a vaild extention to image_path_list
+    for file in os.listdir(imageDir):
+        extension = os.path.splitext(file)[1]
+        image_path_list.append(os.path.join(imageDir, file))
+
+    null = []
+    for image in image_path_list:
+        null.append(cv2.imread(image))
+
+    i=-1
+
+    for image in image_path_list:
+        i += 1
+        null[i]=cv2.resize(cv2.imread(image), dsize=(0, 0), fx=0.2, fy=0.2)
+
+    null1 = []
+    for i in range(int((len(null))**(.5))):
+        null1.append([])
+
+        for j in range(int((len(null))**(.5))-1):
+            null1[i].append(null[i*int((len(null))**(.5)) + j])
 
 
-def output():
-    path = tk.filedialog.askopenfilename()
-    input_entry.delete(1, tk.END)  # Remove current text in entry
-    input_entry.insert(0, path)  # Insert the 'path'
+    def concat_tile(im_list_2d):
+        return cv2.vconcat([cv2.hconcat(im_list_h) for im_list_h in im_list_2d])
+        
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    fullimage = concat_tile(null1)
+    cv2.imwrite(str(timestr)+'.jpg', fullimage)
+    cv2.imshow(str(timestr)+".jpg",fullimage)
+    cv2.waitKey()
+    # plt.imshow(fullimage)
+    # plt.show()
 
 
 top_frame = tk.Frame(master)
 bottom_frame = tk.Frame(master)
 line = tk.Frame(master, height=1, width=400, bg="grey80", relief='groove')
 
-input_path = tk.Label(top_frame, text="Input File Path:")
-input_entry = tk.Entry(top_frame, text="", width=40)
-browse1 = tk.Button(top_frame, text="Browse", command=input)
+input_path = tk.Label(top_frame, text="Search for folder with seperate images:")
+browse = tk.Button(top_frame, text="Browse", command=fullimage_merge)
 
-output_path = tk.Label(bottom_frame, text="Output File Path:")
-output_entry = tk.Entry(bottom_frame, text="", width=40)
-browse2 = tk.Button(bottom_frame, text="Browse", command=output)
-
-begin_button = tk.Button(bottom_frame, text='Begin!')
 
 top_frame.pack(side=tk.TOP)
 line.pack(pady=10)
 bottom_frame.pack(side=tk.BOTTOM)
 
 input_path.pack(pady=5)
-input_entry.pack(pady=5)
-browse1.pack(pady=5)
-
-output_path.pack(pady=5)
-output_entry.pack(pady=5)
-browse2.pack(pady=5)
-
-begin_button.pack(pady=20, fill=tk.X)
-
+browse.pack(pady=5)
 
 master.mainloop()
---
+++ ExampleUI.spec
@@ -0,0 +1,37 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+block_cipher = None
+
+
+a = Analysis(['ExampleUI.py'],
+             pathex=['C:\\Users\\AG_OPTIK\\Documents\\GitHub\\Mapping'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher,
+             noarchive=False)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          [],
+          exclude_binaries=True,
+          name='ExampleUI',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          console=True )
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=True,
+               upx_exclude=[],
+               name='ExampleUI')
